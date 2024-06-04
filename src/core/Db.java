a221568aa4bf8c484dd15f1fb42937e41fb51b71
yasarcandamli <yasarcandamli@gmail.com>
2024-06-04 16:12:08 +0300

    Projenin Klaösr ve Çekirdek Yapısı

---
+++ src/core/Db.java
@@ -0,0 +1,41 @@
+package core;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Db {
+    //Singleton Design Pattern
+    private static Db instance = null;
+    private Connection connection = null;
+    private final String DB_URL = "jdbc:postgresql://localhost:5432/rentacar";
+    private final String DB_USER = "postgres";
+    private final String DB_PASSWORD = "yasarcan";
+
+    private Db() {
+        try {
+            this.connection = DriverManager.getConnection(
+                    this.DB_URL,
+                    this.DB_USER,
+                    this.DB_PASSWORD
+            );
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static Connection getInstance() {
+        try {
+            if (instance == null || instance.getConnection().isClosed()) {
+                instance = new Db();
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return instance.getConnection();
+    }
+}
